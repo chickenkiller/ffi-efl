9715a66064b3e20e9a39f6f209a9abddecbc1192
Lionel Orry <lionel.orry@gmail.com>
2011-04-21 11:10:33 +0200

    remove the Efl:: namespace as well to be even clearer

--- test/elm_win_class.rb
+++ test/elm_win_class.rb
@@ -12,29 +12,29 @@ class MyWin < Elm::ElmWin
         super FFI::MemoryPointer::NULL, name
         win_title_set title
         feed
-        Efl::Evas.object_smart_callback_add @ptr, "delete,request", method(:exit), FFI::MemoryPointer::NULL
+        Evas.object_smart_callback_add @ptr, "delete,request", method(:exit), FFI::MemoryPointer::NULL
     end
     def feed
         @bg = add 'bg'
-        Efl::Evas.object_size_hint_weight_set @bg, 1.0, 1.0
+        Evas.object_size_hint_weight_set @bg, 1.0, 1.0
         resize_object_add @bg
-        Efl::Evas.object_show @bg
+        Evas.object_show @bg
         @lb = add 'label'
-        Efl::Elm.label_label_set @lb, "Hello World!"
-        Efl::Evas.object_size_hint_weight_set @lb, 1.0, 1.0
+        Elm.label_label_set @lb, "Hello World!"
+        Evas.object_size_hint_weight_set @lb, 1.0, 1.0
         resize_object_add @lb
-        Efl::Evas.object_show @lb
+        Evas.object_show @lb
     end
     def exit *args
     	puts "Good bye!"
-        Efl::Elm.exit();
+        Elm.exit();
     end
 end
 #
 win = MyWin.new "App name", "Window Title" do |w|
-    Efl::Evas.object_move w.ptr, 300, 300
-    Efl::Evas.object_resize w.ptr, 200, 100
-    Efl::Evas.object_show w.ptr
+    Evas.object_move w.ptr, 300, 300
+    Evas.object_resize w.ptr, 200, 100
+    Evas.object_show w.ptr
 end
 
 Elm.run
