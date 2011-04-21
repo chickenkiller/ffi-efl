b801d684b6ae2592fb23c7a46d77c43ef703a09c
Lionel Orry <lionel.orry@gmail.com>
2011-04-21 11:07:19 +0200

    Use the method_missing generated helpers to show a different syntax
    Keep the elm_win.rb application using the original API calls so that
    both cases are seen.

--- test/elm_win_class.rb
+++ test/elm_win_class.rb
@@ -12,28 +12,29 @@ class MyWin < Elm::ElmWin
         super FFI::MemoryPointer::NULL, name
         win_title_set title
         feed
-        Efl::API.evas_object_smart_callback_add @ptr, "delete,request", method(:exit), FFI::MemoryPointer::NULL
+        Efl::Evas.object_smart_callback_add @ptr, "delete,request", method(:exit), FFI::MemoryPointer::NULL
     end
     def feed
         @bg = add 'bg'
-        Efl::API.evas_object_size_hint_weight_set @bg, 1.0, 1.0
+        Efl::Evas.object_size_hint_weight_set @bg, 1.0, 1.0
         resize_object_add @bg
-        Efl::API.evas_object_show @bg
+        Efl::Evas.object_show @bg
         @lb = add 'label'
-        Efl::API.elm_label_label_set @lb, "Hello World!"
-        Efl::API.evas_object_size_hint_weight_set @lb, 1.0, 1.0
+        Efl::Elm.label_label_set @lb, "Hello World!"
+        Efl::Evas.object_size_hint_weight_set @lb, 1.0, 1.0
         resize_object_add @lb
-        Efl::API.evas_object_show @lb
+        Efl::Evas.object_show @lb
     end
     def exit *args
-        Efl::API.elm_exit();
+    	puts "Good bye!"
+        Efl::Elm.exit();
     end
 end
 #
 win = MyWin.new "App name", "Window Title" do |w|
-    Efl::API.evas_object_move w.ptr, 300, 300
-    Efl::API.evas_object_resize w.ptr, 200, 100
-    Efl::API.evas_object_show w.ptr
+    Efl::Evas.object_move w.ptr, 300, 300
+    Efl::Evas.object_resize w.ptr, 200, 100
+    Efl::Evas.object_show w.ptr
 end
 
 Elm.run
